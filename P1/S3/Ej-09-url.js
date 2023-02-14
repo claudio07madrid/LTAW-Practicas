4ffbcbd5ad082ed8afe91f3127738f63276271c7
claudio07madrid <claudio07madrid@hotmail.com>
2023-02-14 16:58:43 +0100

    intentos de avance

--- P1/S3/Ej-09-url.js
+++ P1/S3/Ej-09-url.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 console.log("Lectura asíncrona de un fichero");
 
 //-- Realizar la lectura asíncrona
-fs.readFile('fich1.txt','utf8', (err, data) => {
+fs.readFile('fich11.txt','utf8', (err, data) => {
 
     //-- Cuando los datos están ya disponibles
     //-- los mostramos en la consola
