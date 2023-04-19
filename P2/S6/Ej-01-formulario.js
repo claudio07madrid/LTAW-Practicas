91427b4fa761e65605b5f13e9c8027a48fa38c25
claudio07madrid <claudio07madrid@hotmail.com>
2023-04-19 17:46:09 +0200

    Ejemplos S6 terminados

--- P2/S6/Ej-01-formulario.js
+++ P2/S6/Ej-01-formulario.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const PUERTO = 8080;
 
 //-- Cargar pagina web del formulario
-const FORMULARIO = fs.readFileSync('form1.html','utf-8');
+const FORMULARIO = fs.readFileSync('form2.html','utf-8');
 
 //-- HTML de la página de respuesta
 const RESPUESTA = fs.readFileSync('form1-resp.html', 'utf-8');
---
+++ P2/S6/Ej-03-formulario.js
@@ -0,0 +1,54 @@
+//-- Imprimir información sobre la solicitud recibida
+
+const http = require('http');
+const fs = require('fs');
+const PUERTO = 8080;
+
+//-- Cargar pagina web del formulario
+const FORMULARIO = fs.readFileSync('form1.html','utf-8');
+
+//-- HTML de la página de respuesta
+const RESPUESTA = fs.readFileSync('form1-resp2.html', 'utf-8');
+
+
+const server = http.createServer((req, res) => {
+
+    //-- Construir el objeto url con la url de la solicitud
+    const myURL = new URL(req.url, 'http://' + req.headers['host']);  
+  
+    //-- Leer los parámetros
+    let nombre = myURL.searchParams.get('nombre');
+    let apellidos = myURL.searchParams.get('apellidos');
+    console.log(" Nombre: " + nombre);
+    console.log(" Apellidos: " + apellidos);
+    
+  
+    //-- Por defecto entregar formulario
+    let content_type = "text/html";
+    let content = FORMULARIO;
+  
+    if (myURL.pathname == '/procesar') {
+        content_type = "text/html";
+  
+        //-- Reemplazar las palabras claves por su valores
+        //-- en la plantilla HTML
+        content = RESPUESTA.replace("NOMBRE", nombre);
+        content = content.replace("APELLIDOS", apellidos);
+  
+        //-- si el usuario es Chuck Norris se añade HTML extra
+        let html_extra = "";
+        if (nombre=="Chuck" && apellidos=="Norris") {
+           html_extra = "<h2>Chuck Norris no necesita registrarse</h2>";
+        }
+        content = content.replace("HTML_EXTRA", html_extra);
+    }
+  
+    //-- Enviar la respuesta
+    res.setHeader('Content-Type', content_type);
+    res.write(content);
+    res.end()
+  
+  });
+  
+  server.listen(PUERTO);
+  console.log("Escuchando en puerto: " + PUERTO);
