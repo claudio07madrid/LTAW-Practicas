e92be03725bd2aee52fb25275c675b1048ef866f
claudio07madrid <claudio07madrid@hotmail.com>
2023-04-19 19:57:57 +0200

    Avances ccookies

--- P2/tienda.js
+++ P2/tienda.js
@@ -2,13 +2,22 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
+const { ifError } = require('assert');
 
 const port = 9000;
+
+//-- Cargar pagina web del formulario
+const FORMULARIO = fs.readFileSync('formularioUsuarios.html','utf-8');
+
+//-- HTML de la página de respuesta
+const RESPUESTA = fs.readFileSync('form1-resp.html', 'utf-8');
+
 
 console.log(`Servidor iniciado en http://localhost:${port}`);
 
 
 const server = http.createServer((req, res) => {
+  let mainURL = new URL(req.url, "http://" + req.headers["host"]);
   // Obtener la ruta del archivo solicitado
   let filePath = path.join(__dirname, req.url);
   // Obtener solo el nombre del archivo sin la ruta
@@ -19,6 +28,11 @@ const server = http.createServer((req, res) => {
     filePath = path.join(__dirname, '/tienda.html');
   }
 
+  //-- Por defecto entregar formulario
+  let content_type = "text/html";
+  let content = FORMULARIO;
+
+  
   if (req.url === '/productos') {
     // Si se solicita la URL /productos, leer los productos del archivo tienda.json
     fs.readFile(path.join(__dirname, 'tienda.json'), (err, data) => {
@@ -43,6 +57,7 @@ const server = http.createServer((req, res) => {
   console.log(`Se ha solicitado el archivo: http://localhost:${port}/${fileName}`);
 
   // Verificar si el archivo existe en el disco
+  if (fileName.split("?")[0] != "procesar") {
   fs.readFile(filePath, (err, data) => {
     if (err) {
       //Si el archivo solicitado no existe, se comprueba si el error es del tipo ENOENT, que indica que el archivo no existe.
@@ -97,6 +112,40 @@ const server = http.createServer((req, res) => {
       res.end(data);
     }
   });
+}else {
+  console.log("Estamos entrando");
+  let usuarioEncontrado = false;
+  console.log(fileName);
+  usuario = mainURL.searchParams.get('Usuario');
+  contrasena = mainURL.searchParams.get('password');
+  console.log(usuario);
+  console.log(contrasena);
+  archivoJSON = fs.readFileSync("tienda.json");
+  diccionario = JSON.parse(archivoJSON);
+  diccionario["usuarios"].forEach(element => {
+    if(usuario == element["correo"] && contrasena == element["contrasena"]){
+      console.log("si tioooooooooooooooooooooooooooo");
+      usuarioEncontrado = true;
+      return;
+    }
+  });
+  if(usuarioEncontrado){
+    console.log("Usuario encontrado ENVIANDO COOKIE YES UNCLE");
+    res.setHeader("Set-Cookie","user = " + usuario);
+    fs.readFile('tienda.html', function (err, data) {
+      if (err) throw err;
+      let home = data.toString();
+      console.log("hola");
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(home);
+      res.end();
+    });
+} else {
+    // hacer algo si el usuario no se encuentra
+}
+
+}
+
 });
 
 server.listen(port, () => {
